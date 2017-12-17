ce9b8b7604b021d54419a99b84aca1dfbeca8f6b
anamarina <ana.marina@mail.ru>
2017-12-17 21:27:30 +0300

    First reducer (for star2002-full.csv file)

--- task1/reduce_1.py
+++ task1/reduce_1.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     line = i.split('\t')
     max_prodTime = float(line[1])
     min_prodTime = float(line[1])
+    
     # search for true min and max values
     for i in sys.stdin:
         line = i.split('\t')
@@ -13,8 +14,11 @@ if __name__ == '__main__':
             max_prodTime = prodTime
         if min_prodTime > prodTime:
             min_prodTime = prodTime
-    print(min_prodTime)
-    print(max_prodTime)
-    print(min_prodTime + (max_prodTime - min_prodTime) * 0.05)
+    
+    # quantiles
+    print(min_prodTime + (max_prodTime - min_prodTime) * 0.05) 
     print(max_prodTime - (max_prodTime - min_prodTime) * 0.05)
+    
+    # print(min_prodTime)
+    # print(max_prodTime)
     # print(min_prodTime + (max_prodTime - min_prodTime) * 0.05, max_prodTime - (max_prodTime - min_prodTime) * 0.05)
