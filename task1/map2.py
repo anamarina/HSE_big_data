3318d01fb5715a69137125e435d55210003f2c1d
anamarina <ana.marina@mail.ru>
2017-12-26 11:57:10 +0300

    Update and rename map_2.py to map2.py

---
+++ task1/map2.py
@@ -0,0 +1,19 @@
+import sys
+
+file = open('output1.txt', 'r')
+dict_val = dict()
+
+#lines = file.splitlines() #split file into lines without newlines
+#file_rejoined = ''.join(lines)
+
+for line in file:
+    line = line.rstrip('\n') #remove the trailing newline
+    value = line.split("\t")
+    dict_val[value[0]] = value[1]
+file.close()
+
+dict_val
+for line in sys.stdin:
+    columns = line.split(",")   
+    print(columns[0] + "\t" + columns[1] + "," + columns[10] + "," + columns[11] + ',' + dict_val[columns[0]])
+
