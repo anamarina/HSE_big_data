ef5ac95247e2161f960666fcb3c9aabf3871b596
anamarina <ana.marina@mail.ru>
2017-12-26 12:31:26 +0300

    Update map1.py

--- task1/map1.py
+++ task1/map1.py
@@ -8,3 +8,5 @@ import sys
 for line in sys.stdin:
     columns = line.split(",")   
     print(columns[0]+"\t"+columns[10])
+
+# bash: cat star2002-sample.csv | python map1.py |sort | python reduce1.py > output1.txt |
